d6456d253834ffbd70aa4c7c63183dce76aed3cc
Jordan Bean <84806584+jordanbean-msft@users.noreply.github.com>
2022-07-21 14:31:21 -0500

    init (#1)

---
+++ web/Client/GraphApiAuthorizationMessageHandler.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+
+namespace web.Client
+{
+    public class GraphApiAuthorizationMessageHandler : AuthorizationMessageHandler
+    {
+        public GraphApiAuthorizationMessageHandler(IAccessTokenProvider provider, NavigationManager navigationManager) : base(provider, navigationManager)
+        {
+            ConfigureHandler(
+                authorizedUrls: new[] { "https://graph.microsoft.com/" },
+                scopes: new[] { 
+                    "https://graph.microsoft.com/User.Read",  
+                    "https://graph.microsoft.com/Group.Read.All",  
+                    "https://graph.microsoft.com/email", 
+                    "https://graph.microsoft.com/openid", 
+                    "https://graph.microsoft.com/profile" });
+        }
+    }
+}
